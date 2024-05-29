6a4fa22536942883074844cd43a51628e9ba308c
ansekolesnikov <st1mflash@gmail.com>
2024-05-29 17:17:29 +0300

    Initial commit

---
+++ src/main/java/com/akolesnikov/petclinic/controller/PetsController.java
@@ -0,0 +1,67 @@
+package com.akolesnikov.petclinic.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/pet")
+public class PetsController {
+    @GetMapping("/{id}")
+    public Map<String, String> getPet(@PathVariable String id) {
+        Map<String, String> pet = new HashMap<>();
+        pet.put("id", "1");
+        pet.put("name", "sharick");
+        pet.put("type", "dog");
+        pet.put("date", "2020-01-01");
+        pet.put("status", "HEALTH");
+        return pet;
+    }
+
+    @GetMapping
+    public List<Map<String, String>> getPetList() {
+        List<Map<String, String>> petList = new ArrayList<>();
+        Map<String, String> dog = new HashMap<>();
+        dog.put("id", "1");
+        dog.put("name", "sharick");
+        dog.put("type", "dog");
+        dog.put("date", "2020-01-01");
+        dog.put("status", "HEALTH");
+        petList.add(dog);
+
+        Map<String, String> cat = new HashMap<>();
+        cat.put("id", "2");
+        cat.put("name", "kitiket");
+        cat.put("type", "cat");
+        cat.put("date", "2020-02-01");
+        cat.put("status", "HEALTH");
+        petList.add(cat);
+
+        return petList;
+    }
+
+    @PostMapping
+    public Map<String, String> createPet(@RequestBody Map<String, String> pet) {
+        return pet;
+    }
+
+    @PutMapping
+    public Map<String, String> updatePet(@RequestBody Map<String, String> pet) {
+        return pet;
+    }
+
+    @PatchMapping
+    public Map<String, String> updatePetPartially(@RequestBody Map<String, String> pet) {
+        return pet;
+    }
+
+    @DeleteMapping
+    public Map<String, String> deletePet(@RequestBody Map<String, String> petId) {
+        Map<String, String> message = new HashMap<>();
+        message.put("status", "success");
+        return message;
+    }
+}
